fb09271145c69d4c76dfb62cb0a177c9d3899f96
skarunak <senthilkumar.karunakaran@gmail.com>
2016-01-12 18:02:52 -0800

    Update pkt_gen.py

--- python/pkt_gen.py
+++ python/pkt_gen.py
@@ -2,6 +2,7 @@
 import sys
 import pprint
 import os
+import time
 
 try:
   from scapy.all import * 
@@ -20,54 +21,68 @@ class Pktgen:
     self.pkt_list_intf1 = []
     self.pkt_list_intf2 = []
     print "Initialize pkt .."
+    # Cache needed inputs
+    self.iter_cnt = args['iter_cnt']
+    self.count = args['count']
+    self.rx_intf = args['rx_intf']
+
 
   def BuildPkt(self, **args):
     self.iter_cnt = args['iter_cnt']
     self.count = args['count']
 
-    pkt = Ether(dst=args['dmac'], src=args['smac'])/Dot1Q(vlan=args['vlan'])/IP(src='192.0.0.1', dst='193.0.0.1')/TCP(dport=12345)
-    self.pkt_list_intf1.append(pkt)
-
-    if args['2way'] is True:
-       pkt = Ether(dst=args['smac'], src=args['dmac'])/Dot1Q(vlan=args['vlan'])/IP(src='193.0.0.1', dst='192.0.0.1')/TCP(dport=12345)
-       self.pkt_list_intf2.append(pkt)
-
     i = args['count']
     smac = args['smac']
     dmac = args['dmac']
-    vlan = args['vlan']
+    vlan_id = args['vlan']
 
     while i:
        i-=1
-       vlan = args['vlan']
        if args['iter_vlan'] is 1:
 
-          pkt = Ether(dst=args['dmac'], src=args['smac'])/Dot1Q(vlan=vlan)/IP(src='192.0.0.1', dst='193.0.0.1')/TCP(dport=12345)
+          pkt = Ether(dst=args['dmac'], src=args['smac'])/Dot1Q(vlan=vlan_id)/IP(src='192.0.0.1', dst='193.0.0.1')/TCP(dport=12345)
           self.pkt_list_intf1.append(pkt)
 
           if args['2way'] is True:
-             pkt = Ether(dst=args['smac'], src=args['dmac'])/Dot1Q(vlan=vlan)/IP(src='193.0.0.1', dst='192.0.0.1')/TCP(dport=12345)
+             pkt = Ether(dst=args['smac'], src=args['dmac'])/Dot1Q(vlan=vlan_id)/IP(src='193.0.0.1', dst='192.0.0.1')/TCP(dport=12345)
              self.pkt_list_intf2.append(pkt)
 
-          vlan+=1
+          vlan_id+=1
           
     print "Len of tx pkts %d %d " % (len(self.pkt_list_intf1), len(self.pkt_list_intf2))
               
 
-  def SendPkt(self, tx_intf, iter_cnt):
+  def SendPkt(self, id, tx_intf, iter_cnt):
     i = iter_cnt
+    if id is 1:
+      pkt_list = self.pkt_list_intf1
+    else:
+      pkt_list = self.pkt_list_intf2
+
     print "Sending pkt on intf %s %d times" % (tx_intf, iter_cnt)
     while i:
       i-=1
-      for pkt in self.pkt_list_intf1:
+      for pkt in self.pkt_list:
          sendp(pkt,iface=tx_intf,verbose=None) 
 
   def packet_callback(self): pass
     
   def CapPkt(self, rx_intf):
-    #wait_cnt = self.count * self.iter_cnt
-    wait_cnt = 22
-    pkts=sniff(filter="tcp port 12345", count=wait_cnt, timeout=10)
+    wait_cnt = self.count * self.iter_cnt
+    #wait_cnt = 22
+    if rx_intf is self.rx_intf:
+      tcp_port = "12345"
+    else:
+      tcp_port = "12346"
+
+    pkts=sniff(filter="tcp port "+tcp_port, iface=rx_intf, count=wait_cnt+1, timeout=10)
+    count = 1
+    f = open("cap.txt", "w")
+    for p in pkts:
+       f.writelines("Pkt #%d : " % count)
+       f.writelines(p.summary() + '\n')
+       count+=1
+
     print "rcvd %d pkt" % len(pkts)
 
 if __name__ == '__main__':
@@ -93,6 +108,7 @@ if __name__ == '__main__':
       # Child process - Use for Tx
       print "In Child process .."
       # Todo : Wait for receiver to init then send frames
+      time.sleep(1)
       pkt.BuildPkt(**pkt_param)
       pkt.SendPkt(pkt_param['tx_intf'], pkt_param['iter_cnt'])
       os._exit(0)
